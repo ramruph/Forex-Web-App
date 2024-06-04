bc1225befc064c551ce789058b64c892fcf78d83
Shawn Ramrup <shawn@Macbook-Pro-2.local>
2024-06-04 18:51:54 -0400

    heroku fixes

---
+++ src/defs.py
@@ -0,0 +1,7 @@
+API_KEY = '114d20f044110655af93df3ebf34fad6-a2c166c87a6d9daf77fcb6d71f5a1787'
+ACCOUNT_ID = '101-001-10528695-001'
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = { 
+    'Authorization' : f'Bearer {API_KEY}'
+}
